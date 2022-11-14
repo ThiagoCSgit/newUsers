57eb6c1873449c95226838ca6a996c7fcc188d74
ThiagoCSgit <57235729+ThiagoCSgit@users.noreply.github.com>
2022-11-14 10:50:33 -0300

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,76 @@
+const http = require("http");
+const path = require("path");
+
+const express = require("express");
+const fs = require("fs");
+var session = require("express-session");
+const { application } = require("express");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(session({ secret: "abc" }));
+
+// configuraçoes
+app.set("port", process.env.PORT || 3000);
+
+// secção de login
+app.use("/public/acesso-restrito/*", (req, res, next) => {
+  if (req.session.nome) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+});
+
+// secção de criação de usuários
+app.use("/registro", (req, res, next) => {
+  if (req.session.nome) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+});
+
+// artigos estaticos
+app.use(express.static(__dirname));
+
+//start do server
+server.listen(app.get("port"), () => {
+  console.log("server na porta", app.get("port"));
+});
+
+// secção de login 2
+
+app.post("/login", (req, res) => {
+  const usuarioscad = fs.readFileSync("./users.json");
+  const usuariosparse = JSON.parse(usuarioscad);
+  console.log(usuarioscad)
+  var nome = req.body.nomes;
+  var senha = req.body.senha;
+
+  for (var umUsuario of usuariosparse) {
+    if (nome == umUsuario.nome && senha == umUsuario.senha) {
+      req.session.nome = umUsuario;
+      res.send("conectado");
+      return;
+    }
+  }
+  res.send("falhou");
+});
+
+app.post("/cadastro", (req, res) => {
+  const usuarioscad = JSON.parse(fs.readFileSync("./users.json"));
+
+  const newUser = {
+    nome: req.body.nome,
+    senha: req.body.senha
+  }
+  usuarioscad.push(newUser)
+  console.log('newUser:',newUser)
+  fs.writeFileSync("./users.json", JSON.stringify(usuarioscad))
+  req.session.nome = newUser;
+  res.send('conectado')
+})
